8c64fb0a9c86451c23af0396a6b06d7aca6e9e27
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2023-12-15 16:13:22 +0400

    fix arg parsing

--- build_gcc.sh
+++ build_gcc.sh
@@ -11,5 +11,5 @@ die()
 [ $# -eq 2 ] || die "usage: revision image_tag"
 
 revision=$1;shift
-image_tag=$2;shift
+image_tag=$1;shift
 podman build --build-arg revision=$revision -t $image_tag - < Dockerfile

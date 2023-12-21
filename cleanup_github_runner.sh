3f2563503e2de6d23a7ac8996817b21c5a289250
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2023-12-21 10:22:56 +0400

    step message

--- cleanup_github_runner.sh
+++ cleanup_github_runner.sh
@@ -2,27 +2,33 @@
 
 set -euo pipefail
 
-echo "current disk size"
+step()
+{
+    echo "-------------------------------"
+    echo "$@"
+}
+
+step "current disk size"
 df -h .
 
-echo "delete biggest packages"
+step "delete biggest packages"
 # list biggest packages with:
 # dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n
 sudo apt purge -y azure-cli google-cloud-cli microsoft-edge-stable dotnet-* temurin-* google-chrome-stable llvm-* firefox powershell
 df -h .
 
-echo "remove /opt/*"
+step "remove /opt/*"
 sudo rm -rf /opt/*
 df -h .
 
-echo "delete docker images"
+step "delete docker images"
 docker system prune -af
 df -h .
 
-echo "delete android files"
+step "delete android files"
 sudo rm -rf /usr/local/lib/android
 df -h .
 
-echo "delete haskell (ghc) files"
+step "delete haskell (ghc) files"
 sudo rm -rf /usr/local/.ghcup
 df -h .

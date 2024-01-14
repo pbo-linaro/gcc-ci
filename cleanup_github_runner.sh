5ac8b40138e1a8bc1018265bb1fbf0ee81c543fa
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-01-14 09:57:34 +0400

    cleanup github-runner: do not remove biggest package (mirror fail)

--- cleanup_github_runner.sh
+++ cleanup_github_runner.sh
@@ -11,12 +11,6 @@ step()
 step "current disk size"
 df -h .
 
-step "delete biggest packages"
-# list biggest packages with:
-# dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n
-sudo apt purge -y azure-cli google-cloud-cli microsoft-edge-stable dotnet-* temurin-* google-chrome-stable llvm-* firefox powershell
-df -h .
-
 step "remove /opt/*"
 sudo rm -rf /opt/*
 df -h .
